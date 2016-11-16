1170398fbcc57194552925c1b713509ba0f8a31a
paul <paul@thebridgesoft.kr>
2016-11-16 20:36:29 +0900

    add ckeditor module

---
+++ Rose/src/main/webapp/html/egovframework/com/cmm/utl/ckeditor/config.js
@@ -0,0 +1,32 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.filebrowserUploadUrl = APPROVAL_CONTEXT+'/ckupload';
+	config.uiColor = '#FFFFFF';
+	config.language = 'ko';
+	config.toolbar = 'brigeToolbar';
+	config.toolbar_brigeToolbar =
+	[
+	    ['Source','-','Save','NewPage','Preview','-','Templates'],
+	    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+	    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	    ['Form','Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+	    ['BidiLtr','BidiRtl'],
+	    '/',
+	    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+	    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+	    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+	    ['Link','Unlink','Anchor'],
+	    ['Image','Table','HorizontalRule','SpecialChar','PageBreak'],
+	    '/',
+	    ['Styles','Format','Font','FontSize'],
+	    ['TextColor','BGColor'],
+	    ['Maximize', 'ShowBlocks','-','About']
+	];
+};
